59f5d244bc2a89969caa9b198c37f487a4e196a0
shivanandan-dev <shivanandan.dhanasekara@solitontech.com>
2025-04-25 19:12:52 +0530

    feat: EmployeeBonusApp: add functionality to add new manager and functionality to display employees

---
+++ EmployeeBonusApplication/EmployeeBonusApplication/Employee.cs
@@ -0,0 +1,31 @@
+﻿namespace EmployeeBonusApplication {
+    public abstract class Employee {
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the salary of the employee.
+        /// </summary>
+        public decimal Salary { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Employee"/> class.
+        /// </summary>
+        /// <param name="name">The name of the employee.</param>
+        /// <param name="salary">The salary of the employee.</param>
+        public Employee(string name, decimal salary) {
+            Name = name;
+            Salary = salary;
+        }
+
+        /// <summary>
+        /// Calculates and returns the bonus amount.
+        /// </summary>
+        /// <returns>Bonus as a decimal value.</returns>
+        public abstract decimal CalculateBonus();
+
+        /// <summary>
+        /// Prints the details of the employee.
+        /// </summary>
+        public abstract void PrintDetails();
+    }
+}
