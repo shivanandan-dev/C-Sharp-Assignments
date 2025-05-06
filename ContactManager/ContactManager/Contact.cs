2467684e8ef894bb1561955d3082bdde8e2f750b
shivanandan-dev <shivanandan.dhanasekara@solitontech.com>
2025-05-06 09:42:20 +0530

    refactor: remove unnecessary private fields and make use of auto-implemented properties

--- ContactManager/ContactManager/Contact.cs
+++ ContactManager/ContactManager/Contact.cs
@@ -1,14 +1,9 @@
 ﻿namespace ContactManager {
     internal class Contact {
-        string _name;
-        string _phoneNumber;
-        string _email;
-        string _additionalInformation;
-
-        public string Name { get => _name; set => _name = value; }
-        public string PhoneNumber { get => _phoneNumber; set => _phoneNumber = value; }
-        public string Email { get => _email; set => _email = value; }
-        public string AdditionalInformation { get => _additionalInformation; set => _additionalInformation = value; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string AdditionalInformation { get; set; }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Contact"/> class with the specified details.
@@ -18,10 +13,10 @@
         /// <param name="email">The email address of the contact.</param>
         /// <param name="additionalInformation">Additional information about the contact.</param>
         public Contact(string name, string phoneNumber, string email, string additionalInformation) {
-            _name = name;
+            Name = name;
             PhoneNumber = phoneNumber;
-            _email = email;
-            _additionalInformation = additionalInformation;
+            Email = email;
+            AdditionalInformation = additionalInformation;
         }
     }
 }
