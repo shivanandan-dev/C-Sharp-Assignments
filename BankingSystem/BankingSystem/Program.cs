8c502d0baeaf2a3babaaf0944d4a2bb8f84bf75e
shivanandan-dev <shivanandan.dhanasekara@solitontech.com>
2025-05-10 15:47:56 +0530

    refactor: BankingApp: make main function to be standalone

---
+++ BankingSystem/BankingSystem/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace BankingSystem {
+    internal class Program {
+        /// <summary>
+        /// The main entry point of the application.
+        /// </summary>
+        static void Main() {
+            BankAccount account = null;
+
+            Dictionary<int, (string description, Action action)> accountTypeMenuActions = new Dictionary<int, (string, Action)>()
+            {
+                { 1, ("Savings Account", () => BankManager.InitializeAccount(ref account, typeof(SavingsAccount))) },
+                { 2, ("Checking Account", () => BankManager.InitializeAccount(ref account, typeof(CheckingAccount))) },
+                { 3, ("Exit", () => Environment.Exit(0)) }
+            };
+
+            BankManager.HandleMenuAction(accountTypeMenuActions);
+
+            if (account != null) {
+                BankManager.HandleAccountOperations(account);
+            }
+        }
+    }
+}
