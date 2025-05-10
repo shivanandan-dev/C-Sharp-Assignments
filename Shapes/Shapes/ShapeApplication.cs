0db58a9d71e9c610615f96075217a629c4449d97
shivanandan-dev <shivanandan.dhanasekara@solitontech.com>
2025-05-10 14:29:53 +0530

    refactor: ShapeApp: change method documentation

--- Shapes/Shapes/ShapeApplication.cs
+++ Shapes/Shapes/ShapeApplication.cs
@@ -1,8 +1,14 @@
 ﻿namespace ShapesApplication {
     public class ShapeApplication {
         /// <summary>
-        /// Displays the menu options to the user.
+        /// Displays a menu of options to the user.
         /// </summary>
+        /// <param name="menuTitle">The title of the menu to display.</param>
+        /// <param name="menuActions">
+        /// A dictionary where the key represents the menu option number, 
+        /// and the value is a tuple containing the description of the option 
+        /// and the associated action to perform.
+        /// </param>
         public static void DisplayMenu(string menuTitle, Dictionary<int, (string description, Action)> menuActions) {
             Console.WriteLine($"\n{menuTitle}");
             foreach (var menuAction in menuActions) {
