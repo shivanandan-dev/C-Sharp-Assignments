12fffd55567edb58f49101ac448f5c36141faf0f
shivanandan-dev <shivanandan.dhanasekara@solitontech.com>
2025-05-19 12:02:49 +0530

    feat: filter products by electronics greater than 500 and sort it in descending order

---
+++ LanguageIntegratedQuery/LanguageIntegratedQuery/AppInteraction/Application.cs
@@ -0,0 +1,23 @@
+﻿using LanguageIntegratedQuery.IOManager;
+using LanguageIntegratedQuery.Model;
+
+namespace LanguageIntegratedQuery.AppInteraction {
+    internal class Application {
+        public static void HandleMainMenu() {
+            /// <summary>
+            /// Handles the main menu of the application.
+            /// </summary>
+            while (true) {
+                Console.Clear();
+                var mainMenu = new List<Menu> {
+                    new Menu("Task 1", Task1.HandleTask1),
+                    new Menu("Exit", () => Environment.Exit(0))
+                };
+
+                OutputManager.DisplayMenu("Main Menu", mainMenu);
+                int choice = InputManager.GetMenuChoice(mainMenu);
+                mainMenu[choice].Handler.Invoke();
+            }
+        }
+    }
+}
---
+++ LanguageIntegratedQuery/LanguageIntegratedQuery/Model/Menu.cs
@@ -0,0 +1,24 @@
+﻿namespace LanguageIntegratedQuery.Model {
+    public class Menu {
+        /// <summary>
+        /// Gets or sets the description of the menu item.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the action handler associated with the menu item.
+        /// This delegate will be invoked when the menu item is selected.
+        /// </summary>
+        public Action Handler { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Menu"/> class with the specified description and handler.
+        /// </summary>
+        /// <param name="description">The description to display for the menu item.</param>
+        /// <param name="handler">The action to execute when the menu item is selected.</param>
+        public Menu(string description, Action handler) {
+            Description = description;
+            Handler = handler;
+        }
+    }
+}
