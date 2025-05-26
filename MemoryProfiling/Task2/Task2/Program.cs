a4d20e5c0f438149fa5f9d2866659a35e143cd9a
shivanandan-dev <shivanandan.dhanasekara@solitontech.com>
2025-05-26 17:36:16 +0530

    fix: task2: remove memory leak by deallocating the old memory

---
+++ MemoryProfiling/Task2/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task2 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Enter the size of each array (e.g. 1000): ");
+            int bufferSize = int.Parse(Console.ReadLine()!);
+
+            Console.Write("Enter how many arrays to keep in memory (e.g. 100): ");
+            int maxBuffers = int.Parse(Console.ReadLine()!);
+
+            var memAlloc = new List<int[]>();
+
+            Console.WriteLine("\nPress any key to start allocating...");
+            Console.ReadKey();
+
+            while (true) {
+                memAlloc.Add(new int[bufferSize]);
+
+                if (memAlloc.Count > maxBuffers) {
+                    memAlloc.RemoveAt(0);
+                }
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
