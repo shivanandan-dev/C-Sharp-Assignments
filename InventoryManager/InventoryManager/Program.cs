5cda16c933fb088828b2ad6bf30a138b90b3578d
shivanandan-dev <shivanandan.dhanasekara@solitontech.com>
2025-05-28 08:44:37 +0530

    Assignment 3 | Inventory Manager (#3)
    * feat: develop functionality to add new product to the inventory
    
    * feat: develop functionality to view products with sort feature
    
    * feat: develop functionality to search product
    
    * feat: develop functionality to edit and delete products
    
    * refactor: move main function to a standalone program class
    
    * refactor: remove redundant boolean variables
    
    * refactor: remove unnecessary arguments
    
    * refactor: change validator class to static
    
    * refactor: remove unnecessary fields
    
    * refactor: rename Product to ProductDetails
    
    * refactor: rename manager to inventoryManager
    
    * refactor: change variable namings
    
    * refactor: create menu class to maintain menu description and menu handler
    
    * refactor: change getInformation to getProductInformation
    
    * refactor: change GetProductUntilNotExist to GetUniqueProductInput
    
    * refactor: remove unnecessary use of out
    
    * refactor: redefine GetUniqueProductInput method
    
    * change IsProductAlreadyExist to IsProductDuplicate
    
    * refactor: use nameof to avoid use of contants for arguments
    
    * refactor: remove redundant parameters from methods
    
    * refactor: change variable, method and parameter namings
    
    * refactor: Add comments to methods
    
    * feat: develop functionality to display products when adding, editing and deleting products
    
    * fix: handle invalid input for id
    
    * refactor: change method comment and reuse existing code
    
    * refactor: merge Delete and DeleteByAttribute into a single method
    
    * refactor: use HandleMenuAction for main menu to avoid redundancy
    
    * fix: display prompt continuation only once on invalid input in delete method

---
+++ InventoryManager/InventoryManager/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace InventoryManager {
+    public class Program {
+        /// <summary>
+        /// The main entry point of the application. Handles menu navigation and user input for managing products.
+        /// </summary>
+        public static void Main() {
+            InventoryManager inventoryManager = new InventoryManager();
+
+            // NOTE: The following default data is added for testing and debugging purposes only.
+            // These entries should be removed or commented out before deploying the application to production.
+            InventoryManager.Products.Add(new ProductDetails("1", "Oreo Buiscuit", 20.03m, 3));
+            InventoryManager.Products.Add(new ProductDetails("2", "Dairy Milk", 30m, 5));
+            InventoryManager.Products.Add(new ProductDetails("3", "Kit Kat", 40.33m, 2));
+            InventoryManager.Products.Add(new ProductDetails("4", "Bourbon", 50m, 5));
+
+            var mainMenuActions = new List<Menu> {
+                new Menu ( "Add New Product", inventoryManager.AddNewProduct ),
+                new Menu ( "View Products", inventoryManager.ViewProducts ),
+                new Menu ( "Search Product",  inventoryManager.SearchProduct ),
+                new Menu ( "Edit Product",  inventoryManager.EditProduct ),
+                new Menu ( "Delete Product", inventoryManager.DeleteProduct ),
+                new Menu ( "Exit", () => inventoryManager.ExitEnvironment())
+            };
+
+            inventoryManager.HandleMenuActions(mainMenuActions, false, null, true);
+        }
+    }
+}
