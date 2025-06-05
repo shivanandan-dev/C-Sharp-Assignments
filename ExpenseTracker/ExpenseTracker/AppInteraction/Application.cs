21b237aabacc049106882b2d33b8d7edcc1e2046
shivanandan-dev <shivanandan.dhanasekara@solitontech.com>
2025-06-05 15:18:17 +0530

    Assignment 4 | Expense Manager (#4)
    * feat: develop functionality to add new expense
    
    * feat: develop functionality to view expenses
    
    * feat: develop functionality to delete task
    
    * feat: develop functionality to edit expenses
    
    * refactor: add param comments to functions and properties
    
    * fix: change error message on out of range menu option input
    
    * fix: disable console clear expense table when editing a expense
    
    * refactor: move private method below public methods
    
    * refactor: add necessary documentation
    
    * refactor: remove unnecessary method
    
    * refactor: change ExpanseManager file to TransactionManager
    
    * refactor: remove redundant DisplayInvalidInputError method by adding default parameter
    
    * fix: off-by-one error in expense editing

---
+++ ExpenseTracker/ExpenseTracker/AppInteraction/Application.cs
@@ -0,0 +1,55 @@
+﻿using ExpenseTracker.IOManager;
+
+namespace ExpenseTracker.AppInteraction {
+    internal class Application {
+
+        /// <summary>
+        /// Displays a menu and performs the action based on the user's selection.
+        /// </summary>
+        /// <param name="title">The title of the menu to display.</param>
+        /// <param name="menuActions">A dictionary mapping menu options to their respective descriptions and actions.</param>
+        /// <param name="exitOption">The menu option that terminates the loop and exits.</param>
+        /// <param name="clearConsole">Whether to clear the console before displaying the menu.</param>
+        /// <param name="repeat">Whether to repeat the menu after an action is performed.</param>
+        /// <param name="promptForContinuation">Whether to prompt for continuation after each action.</param>
+        public static void DisplayMenuWithActions(
+            string title,
+            Dictionary<int, (string, Action action)> menuActions,
+            int exitOption = 0,
+            bool clearConsole = true,
+            bool repeat = true,
+            bool promptForContinuation = true
+        ) {
+            do {
+                Console.WriteLine();
+                if (clearConsole)
+                    Console.Clear();
+                OutputManager.DisplayMenu(title, menuActions);
+                int choice = InputManager.GetMenuChoice(menuActions);
+                if (choice == exitOption) {
+                    return;
+                }
+                menuActions[choice].action.Invoke();
+                if (promptForContinuation)
+                    InputManager.PromptForContinuation();
+            } while (repeat);
+        }
+
+        /// <summary>
+        /// Displays the main menu of the application and handles user interaction with various options, such as adding, viewing, editing, and deleting transactions, 
+        /// as well as generating a financial summary or exiting the application.
+        /// </summary>
+        public static void HandleMainMenu() {
+            Dictionary<int, (string, Action action)> menuActions = new Dictionary<int, (string, Action)>() {
+                    { 1, ("Add Transaction", TransactionManager.AddTransaction )},
+                    { 2, ("View Transactions", TransactionManager.ViewTransactions )},
+                    { 3, ("Edit Transactions", TransactionManager.EditTransaction )},
+                    { 4, ("Delete Transactions", TransactionManager.DeleteTransaction )},
+                    { 5, ("Financial Summary", () => { })},
+                    { 6, ("Exit", () => { Environment.Exit(0); })}
+                };
+
+            DisplayMenuWithActions("Main Menu", menuActions);
+        }
+    }
+}
---
+++ ExpenseTracker/ExpenseTracker/IOManager/InputManager.cs
@@ -0,0 +1,104 @@
+﻿using ExpenseTracker.Model;
+using ExpenseTracker.Utils;
+
+namespace ExpenseTracker.IOManager {
+    internal class InputManager {
+        /// <summary>
+        /// Prompts the user to press any key to continue and waits for input.
+        /// </summary>
+        public static void PromptForContinuation() {
+            Console.Write("\nPress any key to continue...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Gets a valid menu choice from the user based on the provided menu actions.
+        /// </summary>
+        /// <param name="menuActions">A dictionary of menu options mapping to their descriptions and corresponding actions.</param>
+        /// <returns>The user's selected menu choice as an integer.</returns>
+        public static int GetMenuChoice(Dictionary<int, (string, Action)> menuActions) {
+            Console.WriteLine();
+            do {
+                Console.Write($"[Menu] Enter choice: ");
+                string input = Console.ReadLine();
+                bool isNumber = int.TryParse(input, out int choice);
+                if (isNumber && menuActions.ContainsKey(choice)) {
+                    return choice;
+                } else if (isNumber) {
+                    OutputManager.DisplayInvalidInputError($"Choice should be between (1 - {menuActions.Count + 1})");
+                } else {
+                    OutputManager.DisplayInvalidInputError("Invalid Input");
+                }
+            } while (true);
+        }
+
+        /// <summary>
+        /// Prompts the user to enter a positive decimal amount and validates the input.
+        /// </summary>
+        /// <returns>A valid decimal amount entered by the user.</returns>
+        public static decimal GetAmount() {
+            do {
+                Console.Write("Enter Amount: ");
+                string input = Console.ReadLine();
+                bool isDecimal = decimal.TryParse(input, out decimal amount);
+
+                if (isDecimal && amount > 0) {
+                    return amount;
+                } else if (isDecimal) {
+                    OutputManager.DisplayInvalidInputError("Amount should be a positive decimal");
+                } else {
+                    OutputManager.DisplayInvalidInputError();
+                }
+            } while (true);
+        }
+
+        /// <summary>
+        /// Prompts the user to enter a date in the format dd/mm/yyyy and validates the input.
+        /// </summary>
+        /// <returns>A valid DateTime object representing the entered date.</returns>
+        public static DateTime GetDate() {
+            do {
+                Console.Write("Enter Date (dd/mm/yyyy): ");
+                string input = Console.ReadLine();
+                bool isValidDate = Validator.IsValidDateFormat(input);
+
+                if (isValidDate) {
+                    return DateTime.Parse(input);
+                } else {
+                    OutputManager.DisplayInvalidInputError("Invalid Date Format");
+                }
+            } while (true);
+        }
+
+        /// <summary>
+        /// Prompts the user to enter an expense ID and validates the input.
+        /// </summary>
+        /// <param name="expenseDetails">A list of expense details to validate the entered ID against.</param>
+        /// <returns>The valid expense ID entered by the user, or -1 if the input is invalid.</returns>
+        public static int GetExpenseId(List<ExpenseDetail> expenseDetails) {
+            Console.Write("\nEnter ID: ");
+            string input = Console.ReadLine();
+            bool isNumber = int.TryParse(input, out int id);
+
+            if (isNumber && id < expenseDetails.Count + 1 && id > 0) {
+                return id;
+            } else if (isNumber) {
+                OutputManager.DisplayInvalidInputError($"ID should be between (1 - {expenseDetails.Count + 1})");
+            } else {
+                OutputManager.DisplayInvalidInputError();
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// Prompts the user to enter a category or source based on the provided type.
+        /// </summary>
+        /// <param name="type">The type to display in the prompt (e.g., "Category" or "Source").</param>
+        /// <returns>The string input entered by the user.</returns>
+        public static string GetCategoryOrSource(string type) {
+            Console.Write($"Enter {type}: ");
+            return Console.ReadLine();
+        }
+    }
+}
+
