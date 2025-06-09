e05adcdabecfc7ce0b06d0039fa384355c3d7083
shivanandan-dev <shivanandan.dhanasekara@solitontech.com>
2025-06-09 18:23:25 +0530

    feat: implement Task4

---
+++ AdvancedConcepts/Task4/Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task4 {
+    internal class Program {
+        public static void Main() {
+            var numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var evens = numbers.Where(n => n % 2 == 0);
+
+            var squares = evens.Select(n => {
+                int result = n * n;
+                return result;
+            });
+
+            Console.WriteLine("Squared evens: " + string.Join(", ", squares));
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
